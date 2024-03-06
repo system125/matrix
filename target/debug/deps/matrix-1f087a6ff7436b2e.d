09098b057944daa9139065ce8cb731fefba3b707
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-06 17:25:15 +0545

    created base cargo folder structure

---
+++ target/debug/deps/matrix-1f087a6ff7436b2e.d
@@ -0,0 +1,6 @@
+C:\Users\user\rustApp\matrix\target\debug\deps\libmatrix-1f087a6ff7436b2e.rmeta: src\main.rs src\matrix\mod.rs
+
+C:\Users\user\rustApp\matrix\target\debug\deps\matrix-1f087a6ff7436b2e.d: src\main.rs src\matrix\mod.rs
+
+src\main.rs:
+src\matrix\mod.rs:
---
+++ target/debug/deps/matrix-9440bac57860602c.d
@@ -0,0 +1,6 @@
+C:\Users\user\rustApp\matrix\target\debug\deps\libmatrix-9440bac57860602c.rmeta: src\main.rs src\matrix\mod.rs
+
+C:\Users\user\rustApp\matrix\target\debug\deps\matrix-9440bac57860602c.d: src\main.rs src\matrix\mod.rs
+
+src\main.rs:
+src\matrix\mod.rs:
