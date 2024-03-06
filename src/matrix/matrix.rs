b2ff1d7ffe3126bd1e1dea22c547a4ecce4b9c15
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-06 18:51:59 +0545

    implemented matrix for rust

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,1 +1,35 @@
-mod matrix;
+
+use std::fmt;
+use core::option::Iter;
+
+use super::matrix_row_iter::matrix_row_iter;
+/**
+ * Matrix implemented by using linear array
+ */
+pub struct Matrix {
+    pub values: Vec<i32>,
+    pub height: usize,
+    pub width: usize
+}
+
+impl Matrix {
+    pub fn from(data:Vec<i32>,width:usize,height:usize)->Matrix{
+        Matrix {
+            values: data,
+            height: height,
+            width
+        }
+    }
+
+    pub fn to_iter(&self)-> impl Iterator<Item = &i32> {
+         self.values.iter()
+    }
+    pub fn to_mut_iter(&mut self) -> impl Iterator<Item = &mut i32>{
+        self.values.iter_mut()
+    }
+
+    pub fn to_row_iter<'a> (&'a self) -> matrix_row_iter<'a> {
+        matrix_row_iter::new(&self)
+    }
+}
+ 
