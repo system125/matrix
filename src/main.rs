c84fdf06afba8ce000d46afaeb7372b84f98dec2
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-06 18:54:22 +0545

    matrix to string test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use crate::matrix::Matrix;
+
 mod matrix;
 fn main() {
     println!("Hello, world!");
+    
+    let mat_A = Matrix::from(
+        (1..8).collect(),
+        2,
+        4
+    );
+
+    println!("{:?}",mat_A.to_string())
 }
