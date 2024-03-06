c29cf288740d40cb2d352dcdd775a90fba193e52
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-06 18:51:48 +0545

    implemented display matrix

---
+++ src/matrix/display_matrix.rs
@@ -0,0 +1,19 @@
+
+use super::Matrix;
+use std::fmt;
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"Matrix to string")?;
+
+        for i in self.to_row_iter() {
+            for val in i.iter() {
+                write!(f,"{:?} ",*val)?;
+            }
+            writeln!(f,"")?;
+        }
+
+        Ok(())
+    }
+
+}
